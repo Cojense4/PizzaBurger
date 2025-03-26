09fd8c8a258b9ead7c51fd4716faf8b94a348b7b
Connor Jensen <cjensen32@Connors-MacBook-Air.local>
2025-03-26 14:41:11 -0500

    V0.01.1
    Simple changes, just refactoring files etc.
    
    now will be creating the backend of the project in the coming commits

--- src/main/java/com/prog02/burger/AppController.java
+++ src/main/java/com/prog02/burger/AppController.java
@@ -3,7 +3,7 @@ package com.prog02.burger;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppController {
     @FXML
     private Label welcomeText;
 
